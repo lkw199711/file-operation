f4cd2c1c6957f2ff7d5e39eb50fb367820c513ce
lkw199711 <lkw199711@163.com>
2020-10-08 16:11:22 +0800

    添加项目文件。

---
+++ 文件操作/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace 文件操作
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderSelected = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectFolderBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(617, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // selectFolderBtn
+            // 
+            this.selectFolderBtn.Location = new System.Drawing.Point(682, 31);
+            this.selectFolderBtn.Name = "selectFolderBtn";
+            this.selectFolderBtn.Size = new System.Drawing.Size(51, 23);
+            this.selectFolderBtn.TabIndex = 1;
+            this.selectFolderBtn.Text = "选择";
+            this.selectFolderBtn.UseVisualStyleBackColor = true;
+            this.selectFolderBtn.Click += new System.EventHandler(this.selectFolderBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.selectFolderBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderSelected;
+        private System.Windows.Forms.Button selectFolderBtn;
+    }
+}
+
