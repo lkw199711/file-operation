7e54c6b0eec32a4fc56d3ce3a5a4de1b864ebe9f
lkw199711 <lkw199711@163.com>
2020-10-09 10:26:22 +0800

    feat:根据关键字删除文件

--- 文件操作/Form1.Designer.cs
+++ 文件操作/Form1.Designer.cs
@@ -34,22 +34,28 @@
             this.replaceFileNameBtn = new System.Windows.Forms.Button();
             this.replaceOldText = new System.Windows.Forms.TextBox();
             this.replaceNewText = new System.Windows.Forms.TextBox();
+            this.selectFileBtn = new System.Windows.Forms.Button();
+            this.fileSelected = new System.Windows.Forms.OpenFileDialog();
+            this.fileText = new System.Windows.Forms.TextBox();
+            this.deleteKeyText = new System.Windows.Forms.TextBox();
+            this.batchDeleteBtn = new System.Windows.Forms.Button();
+            this.md5DeleteBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // folderText
             // 
             this.folderText.Location = new System.Drawing.Point(45, 33);
             this.folderText.Name = "folderText";
-            this.folderText.Size = new System.Drawing.Size(617, 21);
+            this.folderText.Size = new System.Drawing.Size(572, 21);
             this.folderText.TabIndex = 0;
             // 
             // selectFolderBtn
             // 
-            this.selectFolderBtn.Location = new System.Drawing.Point(682, 31);
+            this.selectFolderBtn.Location = new System.Drawing.Point(633, 31);
             this.selectFolderBtn.Name = "selectFolderBtn";
-            this.selectFolderBtn.Size = new System.Drawing.Size(51, 23);
+            this.selectFolderBtn.Size = new System.Drawing.Size(75, 23);
             this.selectFolderBtn.TabIndex = 1;
-            this.selectFolderBtn.Text = "选择";
+            this.selectFolderBtn.Text = "选择目录";
             this.selectFolderBtn.UseVisualStyleBackColor = true;
             this.selectFolderBtn.Click += new System.EventHandler(this.selectFolderBtn_Click);
             // 
@@ -57,7 +63,7 @@
             // 
             this.replaceFileNameBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.replaceFileNameBtn.Cursor = System.Windows.Forms.Cursors.Default;
-            this.replaceFileNameBtn.Location = new System.Drawing.Point(263, 83);
+            this.replaceFileNameBtn.Location = new System.Drawing.Point(252, 140);
             this.replaceFileNameBtn.Name = "replaceFileNameBtn";
             this.replaceFileNameBtn.Size = new System.Drawing.Size(105, 59);
             this.replaceFileNameBtn.TabIndex = 2;
@@ -67,23 +73,75 @@
             // 
             // replaceOldText
             // 
-            this.replaceOldText.Location = new System.Drawing.Point(45, 83);
+            this.replaceOldText.Location = new System.Drawing.Point(45, 140);
             this.replaceOldText.Name = "replaceOldText";
             this.replaceOldText.Size = new System.Drawing.Size(187, 21);
             this.replaceOldText.TabIndex = 3;
             // 
             // replaceNewText
             // 
-            this.replaceNewText.Location = new System.Drawing.Point(45, 121);
+            this.replaceNewText.Location = new System.Drawing.Point(45, 178);
             this.replaceNewText.Name = "replaceNewText";
             this.replaceNewText.Size = new System.Drawing.Size(187, 21);
             this.replaceNewText.TabIndex = 4;
+            // 
+            // selectFileBtn
+            // 
+            this.selectFileBtn.Location = new System.Drawing.Point(633, 60);
+            this.selectFileBtn.Name = "selectFileBtn";
+            this.selectFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectFileBtn.TabIndex = 5;
+            this.selectFileBtn.Text = "选择文件";
+            this.selectFileBtn.UseVisualStyleBackColor = true;
+            this.selectFileBtn.Click += new System.EventHandler(this.selectFileBtn_Click);
+            // 
+            // fileSelected
+            // 
+            this.fileSelected.FileName = "openFileDialog1";
+            // 
+            // fileText
+            // 
+            this.fileText.Location = new System.Drawing.Point(45, 62);
+            this.fileText.Name = "fileText";
+            this.fileText.Size = new System.Drawing.Size(572, 21);
+            this.fileText.TabIndex = 6;
+            // 
+            // deleteKeyText
+            // 
+            this.deleteKeyText.Location = new System.Drawing.Point(428, 140);
+            this.deleteKeyText.Name = "deleteKeyText";
+            this.deleteKeyText.Size = new System.Drawing.Size(280, 21);
+            this.deleteKeyText.TabIndex = 7;
+            // 
+            // batchDeleteBtn
+            // 
+            this.batchDeleteBtn.Location = new System.Drawing.Point(428, 176);
+            this.batchDeleteBtn.Name = "batchDeleteBtn";
+            this.batchDeleteBtn.Size = new System.Drawing.Size(126, 23);
+            this.batchDeleteBtn.TabIndex = 8;
+            this.batchDeleteBtn.Text = "根据关键字删除";
+            this.batchDeleteBtn.UseVisualStyleBackColor = true;
+            this.batchDeleteBtn.Click += new System.EventHandler(this.batchDeleteBtn_Click);
+            // 
+            // md5DeleteBtn
+            // 
+            this.md5DeleteBtn.Location = new System.Drawing.Point(575, 176);
+            this.md5DeleteBtn.Name = "md5DeleteBtn";
+            this.md5DeleteBtn.Size = new System.Drawing.Size(133, 23);
+            this.md5DeleteBtn.TabIndex = 9;
+            this.md5DeleteBtn.Text = "根据md5删除";
+            this.md5DeleteBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.md5DeleteBtn);
+            this.Controls.Add(this.batchDeleteBtn);
+            this.Controls.Add(this.deleteKeyText);
+            this.Controls.Add(this.fileText);
+            this.Controls.Add(this.selectFileBtn);
             this.Controls.Add(this.replaceNewText);
             this.Controls.Add(this.replaceOldText);
             this.Controls.Add(this.replaceFileNameBtn);
@@ -104,6 +162,12 @@
         private System.Windows.Forms.Button replaceFileNameBtn;
         private System.Windows.Forms.TextBox replaceOldText;
         private System.Windows.Forms.TextBox replaceNewText;
+        private System.Windows.Forms.Button selectFileBtn;
+        private System.Windows.Forms.OpenFileDialog fileSelected;
+        private System.Windows.Forms.TextBox fileText;
+        private System.Windows.Forms.TextBox deleteKeyText;
+        private System.Windows.Forms.Button batchDeleteBtn;
+        private System.Windows.Forms.Button md5DeleteBtn;
     }
 }
 
--- 文件操作/Form1.cs
+++ 文件操作/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +20,11 @@ namespace 文件操作
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// 文件夹选择按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void selectFolderBtn_Click(object sender, EventArgs e)
         {
             // 文件名选取
@@ -39,6 +44,30 @@ namespace 文件操作
                 folderText.Text = folderPath;
             }
         }
+        /// <summary>
+        /// 文件选择按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void selectFileBtn_Click(object sender, EventArgs e)
+        {
+            // 文件名选取
+            DialogResult result = fileSelected.ShowDialog();
+
+            if (result == DialogResult.OK) //另外一种判断办法 if (df.ShowDialog(this) == DialogResult.OK)   
+            {
+                //将中的数据库目录地址赋于类全局变量数据库根目录   
+                string fileName = fileSelected.FileName;
+
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    lkw.msbox("文件夹路径不能为空");
+                    return;
+                }
+
+                fileText.Text = fileName;
+            }
+        }
 
         private void replaceFileNameBtn_Click(object sender, EventArgs e)
         {
@@ -54,28 +83,119 @@ namespace 文件操作
         /// </summary>
         /// <param name="dir">目录路径</param>
         /// <param name="delname">待删除文件或文件夹名称</param>
-        public void folder_operation(string dir, string key, string newStr)
+        public void folder_operation(string dir,string type, string key0, string key1="")
         {
             // 非文件夹
             if (!Directory.Exists(dir)) return;
 
-            foreach (string fileName in Directory.GetFileSystemEntries(dir))
+            //根据关键字删除
+            if (type == "keyword-delete")
             {
-                // 检索没有关键字
-                if (!fileName.Contains(key)) {
-                    // 递归继续寻找符合文件
-                    folder_operation(fileName, key, newStr);
-                    continue;
+                foreach (string fileName in Directory.GetFileSystemEntries(dir))
+                {
+                    // 删除关键字
+                    string key = key0;
+                    // 检索没有关键字
+                    if (!fileName.Contains(key))
+                    {
+                        // 递归继续寻找符合文件
+                        folder_operation(fileName, "keyword-delete", key);
+                        continue;
+                    }
+
+                    // 删除文件
+                    delete(fileName);
                 }
-                
-                // 如果是目录,调用文件夹方法
-                if (Directory.Exists(fileName)) Directory.Move(fileName, fileName.Replace(key, newStr));
 
-                // 如果是文件,调用文件方法
-                if (File.Exists(fileName)) File.Move(fileName, fileName.Replace(key, newStr));
-
+                return;
             }
 
+            //替换文件名
+            if (type == "replace")
+            {
+                foreach (string fileName in Directory.GetFileSystemEntries(dir))
+                {
+                    // 被替换关键字
+                    string oldStr = key0;
+                    // 替换关键字
+                    string newStr = key1;
+                    // 检索没有关键字
+                    if (!fileName.Contains(oldStr))
+                    {
+                        // 递归继续寻找符合文件
+                        folder_operation(fileName, oldStr, newStr);
+                        continue;
+                    }
+
+                    // 替换文件名
+                    replace(fileName, oldStr, newStr);       
+                }
+
+                return;
+            }
+        }
+
+        /// <summary>
+        /// 获取文件MD5值
+        /// </summary>
+        /// <param name="fileName">文件绝对路径</param>
+        /// <returns>MD5值</returns>
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            try
+            {
+                FileStream file = new FileStream(fileName, FileMode.Open);
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(file);
+                file.Close();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetMD5HashFromFile() fail,error:" + ex.Message);
+            }
+        }
+        /// <summary>
+        /// 根据关键字批量删除文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void batchDeleteBtn_Click(object sender, EventArgs e)
+        {
+            string path = folderText.Text;
+            string key = deleteKeyText.Text;
+            string file = fileText.Text;
+            folder_operation(path, "keyword-delete", key);
+        }
+        /// <summary>
+        /// 删除文件或目录
+        /// </summary>
+        /// <param name="fileName">文件路径</param>
+        public void delete(string fileName)
+        {
+            // 如果是目录,调用文件夹方法
+            if (Directory.Exists(fileName)) Directory.Delete(fileName);
+
+            // 如果是文件,调用文件方法
+            if (File.Exists(fileName)) File.Delete(fileName);
+        }
+        /// <summary>
+        /// 删除文件或目录
+        /// </summary>
+        /// <param name="fileName">文件路径</param>
+        public void replace(string fileName,string oldStr,string newStr)
+        {
+            // 如果是目录,调用文件夹方法
+            if (Directory.Exists(fileName)) Directory.Move(fileName, fileName.Replace(oldStr, newStr));
+
+            // 如果是文件,调用文件方法
+            if (File.Exists(fileName)) File.Move(fileName, fileName.Replace(oldStr, newStr));
         }
     }
 }
